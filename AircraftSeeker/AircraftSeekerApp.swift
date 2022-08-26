9882696b149a843678d205c78be5c9ac3bdeca5e
Marc Schwartz <marcschwartz1990@gmail.com>
2022-08-25 20:51:30 -0400

    Initial Commit

---
+++ AircraftSeeker/AircraftSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AircraftSeekerApp.swift
+//  AircraftSeeker
+//
+//  Created by Marc Schwartz on 8/25/22.
+//
+
+import SwiftUI
+
+@main
+struct AircraftSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AircraftSeeker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AircraftSeeker
+//
+//  Created by Marc Schwartz on 8/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
